b5a3ae161bfd4d2b346b264db48f10ac2588d456
Ognjen Kecina <ognjenkecina@gmail.com>
2022-07-03 20:01:22 +0200

    Creates handleChange and getVisibleFields functions

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,10 @@
 import {useEffect, useState} from "react";
 import formJSON from './sampleData.json';
+import {FormContext} from "./context/FormContext";
 
 function App() {
     const [fields, setFields] = useState();
+    const [step, setStep] = useState(1);
 
     /*Set fields from sampleData.json and adds value property to each object*/
     useEffect(() => {
@@ -16,9 +18,40 @@ function App() {
         }
     }, []);
 
+    /*We go through the fields and take for each field its code and compare it with the code that we passed as id where the function is called, i.e. in the Input component*/
+    /*When the condition is met for that field we take the value and set it inside the value*/
+    const handleChange = (id, event) => {
+        try {
+            let changedField = null;
+            const newFields = fields.map((field) => {
+                const {code} = field;
+                if(id === code){
+                    field['value'] = event.target.value;
+                    changedField = field;
+                }
+                return field;
+            })
+            setFields(newFields);
+        } catch (error) {
+            console.log({error});
+        }
+    };
+
+    const getVisibleFields = () => {
+        let visibleFields = [];
+        visibleFields = fields.filter(field => field.step === step).sort((a, b) => {
+            return a.order - b.order;
+        });
+
+        return visibleFields;
+    }
+
     return (
-        <div className="App">
-        </div>
+        <FormContext.Provider value={{handleChange}}>
+            <div className="App">
+                {getVisibleFields().map((field, index) => <Element key={index} field={field}/>)}
+            </div>
+        </FormContext.Provider>
     );
 }
 
