713439a09b72326b7f032afee6378997ab9ac263
Ognjen Kecina <ognjenkecina@gmail.com>
2022-07-03 19:29:54 +0200

    Sets fields from sampleData.json file

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,25 @@
+import {useEffect, useState} from "react";
+import formJSON from './sampleData.json';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    const [fields, setFields] = useState();
+
+    /*Set fields from sampleData.json and adds value property to each object*/
+    useEffect(() => {
+        if (formJSON.fields && formJSON.fields.length > 0) {
+            setFields(formJSON.fields.map((field) => {
+                return {
+                    ...field,
+                    value: field.defaultValue
+                }
+            }));
+        }
+    }, []);
+
+    return (
+        <div className="App">
+        </div>
+    );
 }
 
 export default App;
